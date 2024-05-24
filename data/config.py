d79642af1943b44e3a47cc0f23d6ac5aa19bd405
Orinbayev <amirxonorinbayev5854@gmail.com>
2024-05-24 17:55:29 +0300

    salom

--- data/config.py
+++ data/config.py
@@ -1,10 +1,12 @@
 from environs import Env
-
+BOT_TOKEN = '6680651911:AAGPtmmPon-HHW_p2T1nTB324nBALwCgEG8'
+ADMINS = '6678521239'
+IP = 'localhost'
 # environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+# env = Env()
+# env.read_env()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
